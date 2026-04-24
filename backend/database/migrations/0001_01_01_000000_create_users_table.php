3a106ecd87ce77c877ca08e60402128a40723c86
Amir3356 <amirsiraj1995@gmail.com>
2026-04-24 15:11:59 +0300

    dttdtette

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,17 +18,6 @@ return new class extends Migration
             $table->timestamps();
         });
 
-     
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
-
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
             $table->morphs('tokenable');
@@ -44,7 +33,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('sessions');
         Schema::dropIfExists('personal_access_tokens');
     }
 };
